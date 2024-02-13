a4700df268dc5ec102ad15595fa37f2216c7f41a
akira21kaji <arkamiyama21@gmail.com[alias]>
2024-02-13 16:45:05 +0900

    課題４のテーブルの形整えるところだけできていない

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,37 +1,46 @@
 import React from "react";
+import "./Modal.css";
 
-function Modal(modalData, { onClose }) {
-  console.log(modalData);
+function Modal({ modalData, onClose }) {
+  // console.log(modalData);
 
   return (
     <>
       <div className="detaileContainer">
         <div className="detailItem">
           <div className="modalImg">
-            <img src={modalData.modalData.sprites.front_default} alt="#" />
-            <img src={modalData.modalData.sprites.back_default} alt="#" />
+            <img src={modalData.sprites.front_default} alt="#" />
+            <img src={modalData.sprites.back_default} alt="#" />
           </div>
-          <h3>{modalData.modalData.name}</h3>
-          <p>
-            {modalData.modalData.abilities.map((ability) => {
-              return <div>{ability.ability.name}</div>;
+          <h3>{modalData.name}</h3>
+          <div>
+            {modalData.abilities.map((ability) => {
+              return (
+                <div key={ability.ability.name}>
+                  <span>
+                    能力属性{ability.index}：{ability.ability.name}
+                  </span>
+                </div>
+              );
             })}
-          </p>
-          <div className="tableStats"></div>
-          {modalData.modalData.stats.map((stat) => {
-            return (
-              <table>
-                <tr>
-                  <th>stat name</th>
-                  <th>base stat</th>
-                </tr>
+          </div>
+
+          <table className="pokemonTable">
+            <thead>
+              <tr>
+                <th>stat name</th>
+                <th>base stat</th>
+              </tr>
+            </thead>
+            {modalData.stats.map((stat) => (
+              <tbody key={stat.stat.name}>
                 <tr>
                   <td>{stat.stat.name}</td>
                   <td>{stat.base_stat}</td>
                 </tr>
-              </table>
-            );
-          })}
+              </tbody>
+            ))}
+          </table>
           <button onClick={onClose}>閉じる</button>
         </div>
       </div>
